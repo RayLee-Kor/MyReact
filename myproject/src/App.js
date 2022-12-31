1c05a7f6443d9941c202f466ab8a8cec825ba3ae
Ray Lee <wooo519@naver.com>
2022-12-31 12:34:42 +0900

    change to ts

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,13 +1,25 @@
-import { Router, Routes, Route } from "react-router-dom";
-import {Main} from "./Page";
+import './css/tailwind.css';     /* Replacing App.css */
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
