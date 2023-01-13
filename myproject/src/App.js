2ddc40fb52d5b65e02319e07d9c3264986d9e487
Ray Lee <wooo519@naver.com>
2023-01-13 18:15:37 +0900

    add modal

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -3,10 +3,6 @@ import {Routes, Route} from "react-router-dom";
 import Header from "./components/common/Header";
 
 import MainPage from "./pages/MainPage";
-import MyPage from "./pages/MyPage";
-import Uploadpage from "./pages/UploadPage";
-import LoadingPage from "./pages/LoadingPage";
-import ResultPage from "./pages/ResultPage";
 
 function App() {
     return(
@@ -14,11 +10,9 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                <Route path="/my-page" element={<MyPage/>}/>
-                <Route path="/upload" element={<Uploadpage/>}/>
-                <Route path="/loading" element={<LoadingPage/>}/>
-                <Route path="/result" element={<ResultPage/>}/>
             </Routes>
         </React.Fragment>
     );
 }
+
+export default App;
--- myproject/src/index.js
+++ myproject/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import MainPage from "./pages/MainPage"
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    // BrowerRouter 적용
+    <BrowserRouter>
+      <MainPage />
+    </BrowserRouter>
 );
